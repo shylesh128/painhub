5a24eb9977285e85180eedc4ea56bd107c32c78e
Shylesh S <shyleshstylo@gmail.com>
2024-02-09 18:11:18 +0530

    Add loading screen and spinner to indicate page loading

--- components/layout/Layout.component.jsx
+++ components/layout/Layout.component.jsx
@@ -7,8 +7,20 @@ import TopAppBar from "../TopAppBar";
 export const Layout = ({ children }) => {
   const { user, logout, loading } = useContext(UserContext);
   const router = useRouter();
+  const [pageLoading, setPageLoading] = useState(false);
 
-  if (loading)
+  const handleRouteChange = () => {
+    setPageLoading(true);
+  };
+
+  const handleRouteChangeComplete = () => {
+    setPageLoading(false);
+  };
+
+  router?.events?.on("routeChangeStart", handleRouteChange);
+  router?.events?.on("routeChangeComplete", handleRouteChangeComplete);
+
+  if (loading || pageLoading)
     return (
       <div className="loadingScreen">
         <div className="loader"></div>
